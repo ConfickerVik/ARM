623c03c97d285c8bb232de5017bfc83badd36261
YaroslavWeb <retro.bot220@gmail.ru>
2020-01-22 14:14:00 +0800

    edit readme + add folder teamplates for FE

--- KRPS/views.py
+++ KRPS/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
-	return render(request, 'base.html')
+	title = 'АРМ организация занятий'
+	return render(request, 'auth/index.html', context={'title': title})
+
+def cabinet(request):
+	lessons = ['1урок', '2урок', '3урок']
+	return render(request, 'cabinet/lessons.html', context={'lessons':lessons})
