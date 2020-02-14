7664ff536d2a9b1312476a93435c384f349431bb
Viktor <oshepkoviktor@gmail.com>
2020-02-14 23:28:00 +0800

    edit table group, lessons, students and add estimation, attendance

--- krps_db.sql
+++ krps_db.sql
@@ -16,6 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `attendance`
+--
+
+DROP TABLE IF EXISTS `attendance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `attendance` (
+  `id_attendance` int(11) NOT NULL AUTO_INCREMENT,
+  `attendance` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `id_lessons` int(11) DEFAULT NULL,
+  `id_student` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_attendance`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attendance`
+--
+
+LOCK TABLES `attendance` WRITE;
+/*!40000 ALTER TABLE `attendance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attendance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `courses`
 --
 
@@ -32,7 +58,7 @@ CREATE TABLE `courses` (
   `comment` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_course`),
   KEY `group_id_idx` (`group_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,8 +67,34 @@ CREATE TABLE `courses` (
 
 LOCK TABLES `courses` WRITE;
 /*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` VALUES (1,'Программирование',2015,'ИрГУПС','ПИ.1-16-1',144,'comment1'),(2,'Математика',2015,'ИрГУПС','ПИ.1-16-1',72,'comment2'),(3,'Алгебра и геометрия',2015,'ИрГУПС','ПИ.1-16-1',188,'comment3'),(4,'Экономика',2015,'ИрГУПС','ПИ.1-16-1',36,'comment4'),(5,'Физика',2015,'ИрГУПС','ПИ.1-16-1',18,'comment5'),(6,'www',2020,'qqqq','eee',NULL,NULL),(7,'a',2,'b','c',NULL,NULL),(15,'Конструирование',2020,'ИрГУПС','ПИ.1-16-1',NULL,NULL),(23,'Конс',2020,'ИрГУПС','ПИ.1-16-1',NULL,NULL),(24,'Надёжность',2020,'ИрГУПС','ПИ.1-15-1',NULL,NULL),(25,'Консtyt',2020,'ИрГУПС','ПИ.1-15-1',NULL,NULL),(26,'efuyf',2013,'ИрГУПС','we4ffw',NULL,NULL),(27,'wefwfw',2013,'ИрГУПС','ПИ.1-15-1',NULL,NULL),(28,'iortuo',2019,'ИрГУПС','werwe',NULL,NULL),(29,'Конс',2020,'ИрГУПС','ПИ.1-16-1',NULL,NULL),(30,'efe',2323,'efe','efe',NULL,NULL);
+INSERT INTO `courses` VALUES (1,'Программирование',2015,'ИрГУПС','ПИ.1-16-1',144,'comment1'),(2,'Математика',2015,'ИрГУПС','ПИ.1-16-1',72,'comment2'),(3,'Алгебра и геометрия',2015,'ИрГУПС','ПИ.1-16-1',188,'comment3'),(4,'Экономика',2015,'ИрГУПС','ПИ.1-16-1',36,'comment4'),(5,'Физика',2015,'ИрГУПС','ПИ.1-16-1',18,'comment5'),(62,'Тестирование',2020,'ВАНЯ ЛОХ','ПИС',56,'Курс такое себе конечно');
 /*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estimation`
+--
+
+DROP TABLE IF EXISTS `estimation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estimation` (
+  `id_estimation` int(11) NOT NULL AUTO_INCREMENT,
+  `estimation` varchar(45) DEFAULT NULL,
+  `id_lesson` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `id_student` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_estimation`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estimation`
+--
+
+LOCK TABLES `estimation` WRITE;
+/*!40000 ALTER TABLE `estimation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estimation` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -67,7 +119,7 @@ CREATE TABLE `group` (
 
 LOCK TABLES `group` WRITE;
 /*!40000 ALTER TABLE `group` DISABLE KEYS */;
-INSERT INTO `group` VALUES (1,'ПИ-15','ИрГУПС',20),(2,'ИС-15','ИрГУПС',25),(3,'БИ-15','ИрГУПС',30);
+INSERT INTO `group` VALUES (1,'ПИ.1-16-1','ИрГУПС',20),(2,'ИС-15','ИрГУПС',25),(3,'БИ-15','ИрГУПС',30);
 /*!40000 ALTER TABLE `group` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,12 +132,12 @@ DROP TABLE IF EXISTS `lessons`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lessons` (
   `id_lesson` int(11) NOT NULL AUTO_INCREMENT,
-  `id_test` int(11) NOT NULL,
+  `id_course` int(11) NOT NULL,
   `type` varchar(45) NOT NULL,
   `date` date NOT NULL,
   `name` varchar(60) NOT NULL,
   PRIMARY KEY (`id_lesson`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +146,7 @@ CREATE TABLE `lessons` (
 
 LOCK TABLES `lessons` WRITE;
 /*!40000 ALTER TABLE `lessons` DISABLE KEYS */;
-INSERT INTO `lessons` VALUES (1,10,'лаб','2015-01-05','Лаб 1'),(2,20,'лек','2015-01-06','Лек 1'),(3,30,'лек','2015-01-07','Лек 1');
+INSERT INTO `lessons` VALUES (1,1,'лаб','2015-01-05','Лаб 1'),(2,2,'лек','2015-01-06','Лек 1'),(3,3,'лек','2015-01-07','Лек 1'),(4,4,'лек','2015-01-07','Лек 1'),(5,3,'лек','2015-01-07','Лек 1'),(6,2,'лек','2015-01-07','Лек 1'),(7,1,'лаб','2015-01-07','Лаб 2'),(8,3,'лаб','2015-01-07','Лаб 1'),(9,4,'лаб','2015-01-07','Лаб 1'),(10,2,'лек','2015-01-07','Лек 2'),(12,1,'Практическое занятие','2020-02-14','В мире птиц');
 /*!40000 ALTER TABLE `lessons` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,10 +192,7 @@ CREATE TABLE `students` (
   `lastname_stud` varchar(45) NOT NULL,
   `firstname_stud` varchar(45) NOT NULL,
   `middlename_stud` varchar(45) NOT NULL,
-  `attend_stud` varchar(1) NOT NULL,
   `group_stud` varchar(15) NOT NULL,
-  `val_stud` varchar(1) NOT NULL,
-  `date` date NOT NULL,
   PRIMARY KEY (`id_students`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -154,7 +203,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'Антонов','Владимир','Сергеевич','1','ПИ-15','5','2015-01-01'),(2,'Афонина','Светлана','Алексеевна','1','ПИ-15','4','2015-01-01'),(3,'Борисова','Ольга','Олеговна','1','ПИ-15','4','2015-01-01'),(4,'Бандера','Степан','Андреевич','1','ПИ-15','5','2015-01-01'),(5,'Масхадов','Аслан','Алиевич','1','ПИ-15','5','2015-01-01'),(6,'Кадыров','Рамзан','Ахматович','1','ПИ-15','1','2015-01-01'),(7,'Дениев','Якуб','Ильясович','0','ИС-15','5','2015-01-01'),(8,'Павлов','Сергей','Борисович','1','ИС-15','4','2015-01-01'),(9,'Иванова','Дарья','Викторовна','1','ИС-15','4','2015-01-01'),(10,'Остапов','Борис','Геннадьевич','1','ИС-15','5','2015-01-01'),(11,'Борисов','Руслан','Николаевич','1','БИ-15','5','2015-01-02'),(12,'Иванов','Олег','Викторович','1','БИ-15','5','2015-01-02');
+INSERT INTO `students` VALUES (1,'Антонов','Владимир','Сергеевич','ПИ.1-16-1'),(2,'Афонина','Светлана','Алексеевна','ПИ.1-16-1'),(3,'Борисова','Ольга','Олеговна','ПИ.1-16-1'),(4,'Бандера','Степан','Андреевич','ПИ.1-16-1'),(5,'Масхадов','Аслан','Алиевич','ПИ.1-16-1'),(6,'Кадыров','Рамзан','Ахматович','ПИ.1-16-1'),(7,'Дениев','Якуб','Ильясович','ИС-15'),(8,'Павлов','Сергей','Борисович','ИС-15'),(9,'Иванова','Дарья','Викторовна','ИС-15'),(10,'Остапов','Борис','Геннадьевич','ИС-15'),(11,'Борисов','Руслан','Николаевич','БИ-15'),(12,'Иванов','Олег','Викторович','БИ-15');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -195,4 +244,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-01  1:14:48
+-- Dump completed on 2020-02-14 23:25:58
